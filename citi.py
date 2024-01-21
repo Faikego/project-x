6fb88f6414b9ece24aea2b9d50e015e3dbb31d05
faikego <f4llno@yandex.ru>
2024-01-21 18:21:34 +0400

    Это работает, вроде...

--- citi.py
+++ citi.py
@@ -2,7 +2,8 @@ from datetime import time
 import time
 from selenium.webdriver.common.by import By
 from browser_helpers import browser_init
-
+from selenium.webdriver.common.keys import Keys
+target='Пылесос'
 def citi_search_and_select (target):
     '''
     Функция по поиску нужного товара в Ситилинке, возвращает название товара и его ценник
@@ -10,18 +11,19 @@ def citi_search_and_select (target):
     :return:
     '''
     driver = browser_init('https://www.citilink.ru')
-    element = driver.find_element(By.CSS_SELECTOR,'#__next > div > header > div.fresnel-container.fresnel-lessThan-tabletL > div.css-1ck99ji.es4sr930 > div > button:nth-child(3) > span').click()
-    element = driver.find_element(By.CSS_SELECTOR, '#__next > div > div.css-1c45yj0.e1p9cxpz0 > div > div.css-1ck99ji.es4sr930 > div > div > div.css-i9gxme.e10t34ck0 > form > div > div > label > input')
-    element.send_keys(target)
-    element = driver.find_element(By.CSS_SELECTOR, '#__next > div > div.css-1c45yj0.e1p9cxpz0 > div > div.css-1ck99ji.es4sr930 > div > div > div.css-i9gxme.e10t34ck0 > form > div > div > label > div > div > button:nth-child(2) > span').click()
-    time.sleep(2)
-    element = driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-sp8nf.e1loosed0 > div.egwx31q0.app-catalog-oig9jo.e1loosed0 > div.app-catalog-14dnwby.ehj6uxk0 > div.fresnel-container.fresnel-lessThan-tabletL > div > button').click()
-    element = driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-sp8nf.e1loosed0 > div.egwx31q0.app-catalog-oig9jo.e1loosed0 > div.app-catalog-14dnwby.ehj6uxk0 > div.fresnel-container.fresnel-lessThan-tabletL > div > div > div > div > button:nth-child(2)').click()
-    element = driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-sp8nf.e1loosed0 > div.egwx31q0.app-catalog-oig9jo.e1loosed0 > div.app-catalog-14dnwby.ehj6uxk0 > div.fresnel-container.fresnel-greaterThanOrEqual-tabletL > div > button:nth-child(2)')
+    #time.sleep(20)
+    element = driver.find_element(By.CSS_SELECTOR,'.css-hyxlzm')
     element.click()
-    time.sleep(5)
-    element = driver.find_element(By.CSS_SELECTOR, '#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-sp8nf.e1loosed0 > div.ehanbgo0.app-catalog-1yt54f0.e1loosed0 > div:nth-child(1) > div > div > div.app-catalog-gt3vul.eyol3820 > div.app-catalog-npkzzu.e6iq6rb0 > button > span > div.app-catalog-12y5rbz.elmcou80 > span > span > span.e1j9birj0.e106ikdt0.app-catalog-175fskm.e1gjr6xo0')
-    print(element.text)
-target='Infinix'
-citi_search_and_select(target)
-
+    time.sleep(0.2)
+    element.send_keys(target)
+    time.sleep(0.2)
+    element.send_keys(Keys.ENTER);
+    time.sleep(2)
+    element=driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-1kzfd3s.e1loosed0 > div.egwx31q0.app-catalog-1v1smf8.e1loosed0 > div.app-catalog-14dnwby.ehj6uxk0 > div.fresnel-container.fresnel-greaterThanOrEqual-tabletL > div > button:nth-child(2)').click()
+    time.sleep(0.5)
+    price=driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-1kzfd3s.e1loosed0 > div.ehanbgo0.app-catalog-1rygk07.e1loosed0 > div:nth-child(1) > div > div > div.app-catalog-gt3vul.eyol3820 > div.app-catalog-npkzzu.e6iq6rb0 > button > span > div.app-catalog-12y5rbz.elmcou80 > span > span > span.e1j9birj0.e106ikdt0.app-catalog-56qww8.e1gjr6xo0').text
+    name=driver.find_element(By.CSS_SELECTOR,'#__next > div > main > div.app-catalog-c3ky1w.eg0q2u20 > div > div.app-catalog-1pwu1hf.ewjft870 > section > div.e12wdlvo0.app-catalog-1kzfd3s.e1loosed0 > div.ehanbgo0.app-catalog-1rygk07.e1loosed0 > div:nth-child(1) > div > div > div.app-catalog-632im3.e1sv2xw70 > div.app-catalog-oacxam.e1xes8vl0').text
+    return price,name
+price,name=citi_search_and_select(target)
+print('Название-',name)
+print('Цена-',price)
--- nix.py
+++ nix.py
@@ -1,22 +1,9 @@
 from datetime import time
 import time
 from selenium.webdriver.common.by import By
-from browser_helpers import browser_init
+from browser_helpers import browser_init,list_normalizer
 
 
-# def browser_init ():
-#     '''
-#     Временная функция, удалить после "Слияния"
-#     :return:
-#     '''
-#
-#     service = Service(executable_path='chromedriver\chromedriver.exe')
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')
-#     driver = webdriver.Chrome(service=service)
-#     driver.implicitly_wait(50)
-#     driver.get('https://www.nix.ru')
-#     return driver
 
 def nix_search_and_select(target):
     '''
@@ -26,20 +13,19 @@ def nix_search_and_select(target):
     :return:
     '''
     driver = browser_init('https://www.nix.ru')
-    counter = 0
-    price_lib=[]
     element = driver.find_element(By.CSS_SELECTOR, '#textfield')
     element.send_keys(target)
     element = driver.find_element(By.CSS_SELECTOR, '#search_button').click()
     time.sleep(2)
-    element_library = driver.find_elements (By.CSS_SELECTOR,'#search_results')
-    for i in element_library:
-        counter=counter+1
-        #print(i.find_element(By.CSS_SELECTOR,'#search_results > tbody > tr:nth-child(5)').text)
-        #price_lib.append(i.find_element(By.CSS_SELECTOR,'#search_results > tbody > tr:nth-child('+str(counter)+') > td:nth-child(6)').text)
-        print(counter)
-        print(i.text)
-    return False
+    element = driver.find_element(By.CSS_SELECTOR,'.cell-price-top').click()
+    time.sleep(0.3)
+    price_list = driver.find_elements(By.CSS_SELECTOR,'.cell-half-price')
+    price=price_list[3].text
+    name = driver.find_element(By.CSS_SELECTOR,'.cell-name').text
 
-target='infinix'
-nix_search_and_select(driver,target)
+    return price,name
+
+target="Коммутатор"
+min_price,min_name=nix_search_and_select(target)
+print('Название-',min_name)
+print('Цена-', min_price)
