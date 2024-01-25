39ba51553e4e17492dbdd4220ff7a29ab7aaebd5
2 <kolya321lol654@gmail.com>
2024-01-25 16:22:17 +0400

    new

--- Css.py
+++ Css.py
@@ -2,13 +2,18 @@ css_search_zeon = 'body > div.site-wrapper > div > header > div > div.site-heade
 css_search_zeon_click = 'body > div.site-wrapper > div > header > div > div.site-header-menu.if-size-pc > div.site-header-menu-main > div.site-header-search > div > form > button'
 css_product_zeon = 'body > div.site-wrapper > div > div.site-content > div > div.laypart-main > div > div > div.catalog-browser-content > div > div > div > div:nth-child(1) > article > div.catalog-item-body > div.catalog-item-except-picture > div.catalog-item-info > a'
 css_price_zeon = 'body > div.site-wrapper > div > div.site-content > div > div.laypart-main > div > div > div.catalog-browser-content > div > div > div > div:nth-child(1) > article > div.catalog-item-body > div.catalog-item-except-picture > div.catalog-item-shop > div.if-size-pc > div > div.catalog-item-price-main > span'
+
 css_sity = 'body > div.site-wrapper > div > header > div > div.site-header-menu.if-size-pc > div.site-header-menu-main > div.site-header-select-city-wrapper > div > div.dropdown-current.site-header-select-city-icon.with-icon-map-point > span > span'
 css_mysity = 'body > div.site-wrapper > div > header > div > div.site-header-menu.if-size-pc > div.site-header-menu-main > div.site-header-select-city-wrapper > div > div.dropdown-menu > ul > li:nth-child(4) > a > span'
-
+css_sity_dns_click = '#header-desktop > div > div > div > ul > li:nth-child(1) > div > div.city-select_cKY > div > span.city-select__text_90n'
+css_sity_search = '#select-city > div:nth-child(1) > div > div > div > div > div > input'
+css_sity_search_click = '#select-city > div.city-select-modal_lists-column_ufMaj > ul > li > a > span > span'
 css_search_dns = '#header-search > div > div.presearch.header-presearch.header-bottom__presearch-wrapper > div.presearch__wrapper > input'
-css_search_dns_click = '#header-search > div > div.presearch.header-presearch.header-bottom__presearch-wrapper > div.presearch__wrapper > div.presearch__controls > span.presearch__icon-search > svg'
+css_search_dns_click = '#header-search > div > div.presearch.header-presearch.header-bottom__presearch-wrapper > div.presearch__wrapper > div.presearch__controls > span.presearch__icon-search'
 css_product_dns = '#search-results > div.products-list > div > div:nth-child(2) > div:nth-child(1) > a'
-css_price_dns = '#search-results > div.products-list > div > div:nth-child(2) > div:nth-child(1) > div.product-buy.product-buy_one-line.catalog-product__buy > div > div.product-buy__price'
+css_price_dns = '#pc-IIup > div.product-card-top.product-card-top_full > div.product-card-top__buy > div.product-buy.product-buy_one-line > div > div.product-buy__price'
+
+
 
 
 
--- pars_dns.py
+++ pars_dns.py
@@ -1,25 +1,33 @@
-from datetime import time
 import time
 
-from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-
-from Css import css_search_dns, css_search_dns_click, css_product_dns, css_price_dns
+from Css import (css_search_dns, css_sity_dns_click, css_sity_search, css_sity_search_click, css_search_dns_click,
+                 css_product_dns, css_price_dns)
 
 
 def parser_dns():
-    browser = webdriver.Chrome()
+    browser = uc.Chrome()
     browser.get('https://www.dns-shop.ru')
-    time.sleep(1)
-    browser.find_element(By.CSS_SELECTOR, css_search_dns).send_keys('g 102')
+    time.sleep(0.3)
+    browser.find_element(By.CSS_SELECTOR, css_sity_dns_click).click()
+    time.sleep(0.3)
+    browser.find_element(By.CSS_SELECTOR, css_sity_search).send_keys('Сарапул')
+    browser.find_element(By.CSS_SELECTOR, css_sity_search_click).click()
+    time.sleep(0.3)
+    browser.find_element(By.CSS_SELECTOR, css_search_dns).send_keys('macbook air')
+    time.sleep(0.3)
     browser.find_element(By.CSS_SELECTOR, css_search_dns_click).click()
     product = browser.find_elements(By.CSS_SELECTOR, css_product_dns)
     link = None
     for i in product:
         link = i.get_attribute('href')
-    price = browser.find_element(By.CSS_SELECTOR, css_price_dns).text
+    time.sleep(0.3)
+    price = browser.find_element(By.CSS_SELECTOR, '#search-results > div.products-list > div > div.catalog-products.view-simple > div:nth-child(1) > div.product-buy.product-buy_one-line.catalog-product__buy > div > div.product-buy__price').text
 
     return link, price
 
 
 print(parser_dns())
+
+
