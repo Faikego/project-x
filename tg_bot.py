5f7a0ad8366ac28d7afda673664f961ab48b0151
faikego <f4llno@yandex.ru>
2024-02-19 21:20:09 +0400

    Это работает, вроде...

---
+++ tg_bot.py
@@ -0,0 +1,26 @@
+import telebot
+from initializer import initializer
+token_telega=open('token','r')
+bot = telebot.TeleBot(*token_telega)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = ("Введите '/search название товара котрый хотите найти.'")
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['search'])
+def search(message):
+
+    keywords = message.text[8:]
+
+    if not keywords:
+        bot.reply_to(message, "Пожалуйста, укажите ключевые слова для поиска")
+    else:
+        bot.reply_to(message, 'Поиск займет около 2 минут')
+        bot.reply_to(message, initializer(keywords))
+
+
+bot.polling(none_stop=True)
+
+
