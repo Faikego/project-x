9042903036f48029e42dbbcdf00d61d5bacdca06
faikego <f4llno@yandex.ru>
2024-02-21 22:12:07 +0400

    Это работает, вроде...

---
+++ nix.py
@@ -0,0 +1,40 @@
+from datetime import time
+import time
+from selenium.webdriver.common.by import By
+from browser_helpers import browser_init
+from constants import *
+from selenium.common.exceptions import NoSuchElementException
+
+def parser_nix(target):
+    '''
+    Функция по поиску искомого в никсе, парсит все достижимые для него ценники в словарь (Название+ценник) и выбирает наименьшую сумму
+    Принимает название товара
+    Возвращает пару сумма+имя
+    :param target:
+    :return:
+    '''
+    try:
+        driver = browser_init('https://www.nix.ru')
+        element = driver.find_element(By.CSS_SELECTOR, css_search_nix)
+        element.send_keys(target)
+        element = driver.find_element(By.CSS_SELECTOR, css_search_button_nix).click()
+        time.sleep(5)
+        element = driver.find_element(By.CSS_SELECTOR,css_sorter_button_nix).click()
+        time.sleep(0.3)
+        price_list = driver.find_elements(By.CSS_SELECTOR,css_price_list_nix)
+        price=price_list[3].text
+        name = driver.find_element(By.CSS_SELECTOR,css_name_nix).text
+        unsorted_links=driver.find_elements(By.CSS_SELECTOR, css_link_nix)
+        link = unsorted_links[0].get_attribute('href')
+        text = (
+                    "НАЗВАНИЕ ТОВАРА-" + str(name) +
+                    "\nЦЕНА-" + str(price) +
+                    "\nМАГАЗИН-" + str('Никс') +
+                    "\nССЫЛКА- " + str(link)
+            )
+    except NoSuchElementException:
+        driver.close()
+        text=''
+        return text
+    return text
+
