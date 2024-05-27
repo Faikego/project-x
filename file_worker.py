f7c37b019201dd76cc68a73a4e6031d5fa5cc456
f4lln <f4llno@yandex.ru>
2024-05-27 15:51:52 +0400

    Работа

--- file_worker.py
+++ file_worker.py
@@ -1,4 +1,6 @@
 import statistics
+import psycopg2
+from psycopg2 import sql
 def avg_null_destroyer (destroyer_list):
     '''
     Функция по получению средней цены из списка с предварительным удалением None из листа
@@ -12,8 +14,45 @@ def avg_null_destroyer (destroyer_list):
     avg_price=statistics.mean(destroyer_list)
     return destroyer_list,avg_price
 
-#def sql_appender(item,avg_price,price_list):
+def sql_creator():
+    '''
+    Открывает соединение с базой данных
+    После чего создаёт таблицу Dinfo с столбцами id, item,avg_price,price_list
+    В конце закрывает соединение
+    :return:
+    '''
+    connector= psycopg2.connect(dbname='diplom_db', user='db_user',
+                        password='P@$$w0rd', host='localhost')
+    cursor = connector.cursor()
+    cursor.execute('CREATE table Dinfo (\n'
+                   'id BIGSERIAL PRIMARY KEY,\n'
+                   'item VARCHAR(50),\n'
+                   'avg_price VARCHAR(50),\n'
+                   'price_list VARCHAR(50);\n'
+                   )
+    del connector
 
+def sql_appender (item,avg_price,price_list):
+    '''
+    Добавляет строки в БД
+    :param item:
+    :param avg_price:
+    :param price_list:
+    :return:
+    '''
+    price_list=str(price_list)
+    connector = psycopg2.connect(dbname='diplom_db', user='db_user',
+                                 password='P@$$w0rd', host='localhost')
+    with connector.cursor() as cursor:
+        connector.autocommit = True
+        values = [
+            (item, avg_price, price_list),
+        ]
+        insert = sql.SQL('INSERT INTO Dinfo (item, avg_price, price_list) VALUES {}').format(
+            sql.SQL(',').join(map(sql.Literal, values))
+        )
+        cursor.execute(insert)
+    del connector
 
 def list_separator(list):
     list = str(list)
--- initializer.py
+++ initializer.py
@@ -2,8 +2,14 @@ from zeon import parser_zeo
 from citi import parser_citi
 from nix import parser_nix
 from center import parser_center
-from file_worker import avg_null_destroyer
+from file_worker import avg_null_destroyer,sql_appender
 def initializer (target):
+    '''
+    Проводит инициализацию всех парсеров + добавляет в ДБ вычисленные данные
+    Возвращает текст для бота
+    :param target:
+    :return text:
+    '''
     zeo_text,zeo_price=parser_zeo(target)
     nix_text,nix_price=parser_nix(target)
     citi_text,citi_price=parser_citi(target)
@@ -11,12 +17,13 @@ def initializer (target):
     price_list=[]
     price_list.append(zeo_price,nix_price,citi_price,center_price)
     price_list,avg_price=avg_null_destroyer(price_list)
+    sql_appender(target,avg_price,price_list)
     text = (
         zeo_text+
         nix_text+
         citi_text+
         center_text
     )
-    return text,price_list,avg_price
+    return text
 
 
